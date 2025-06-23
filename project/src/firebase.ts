580855d0520d1e72a5ab2bab577c66daf72c83fe
Nithya Sri S <149350190+Nithya2900@users.noreply.github.com>
2025-06-23 09:24:20 +0530

    firebase.ts

--- project/src/firebase.ts
+++ project/src/firebase.ts
@@ -3,16 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCq-CPy2JtfuduA5xgVyuMLtlAKk4I1I6A",
-  authDomain: "autopolicy-afcb3.firebaseapp.com",
-  projectId: "autopolicy-afcb3",
-  storageBucket: "autopolicy-afcb3.appspot.com",
-  messagingSenderId: "27951306700",
-  appId: "1:27951306700:web:d1d418a12e60858c38ef69",
-  measurementId: "G-PJM73XFEPY"
-};
+//apikeys
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
